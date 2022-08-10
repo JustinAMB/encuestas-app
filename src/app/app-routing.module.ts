9c5e393543dc6b55293672ad520f11902a3a3353
JustinAMB <Justinmb@outlook.es>
2022-08-10 00:13:50 -0600

    se implementa el authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },{
+
+
+
+
+
+
+
+
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardModule ),
+    canActivate: [ AuthGuard ],
+    canLoad: [ AuthGuard ]
+
   }
 
 ];
