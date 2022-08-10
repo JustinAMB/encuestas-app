485da43e0e77c19f988ed7ec4c6376eecf020c63
JustinAMB <Justinmb@outlook.es>
2022-08-10 00:02:06 -0600

    se crea el guard para validar si esta logueado

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private router: Router , private auth:AuthService) { }
+
+  canLoad(route: Route, segments: UrlSegment[]): boolean | Observable<boolean>  {
+    return this.auth.validate().pipe(
+      tap((resp)=>{
+
+        if(!resp){
+          this.router.navigate(['/auth']);
+        }
+      })
+    );
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot):  Observable<boolean> | boolean {
+      return this.auth.validate().pipe(
+        tap((resp)=>{
+  
+          if(!resp){
+            this.router.navigate(['/auth']);
+          }
+        })
+      );
   }
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
+  
 }
